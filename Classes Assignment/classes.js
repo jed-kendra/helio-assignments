4774bf65bd93dc89b5601e105950a344cdc46221
Jed Hanson <jedhanson@Jeds-MacBook-Air.local>
2020-02-10 13:00:42 -0700

    Add classes assignment

---
+++ Classes Assignment/classes.js
@@ -0,0 +1,45 @@
+class Animal {
+    constructor() {
+
+    }
+
+    speak (){
+        console.log(this.sound)
+    }
+
+    sayName () {
+        console.log (this.name)
+    }
+}
+
+class Dog extends Animal {
+
+    constructor (name, sound) {
+        super(Animal)
+        this._name = name
+        this.sound = sound
+    }
+
+    get name() {
+        return this._name
+    }
+
+    set name(newValue) {
+        this._name = newValue
+    }
+
+}
+
+let newDog = new Dog("Westie", "Bark")
+console.log(newDog)
+myDog.speak()
+myDog.name = "Max"
+myDog.sayName()
+
+
+
+    
+
+
+
+
